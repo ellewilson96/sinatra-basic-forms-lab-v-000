ff8bd89734fbaea1aeb0b9b9fd585bda15a16853
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-12 01:51:36 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,9 @@
-class Puppy 
+class Puppy
   attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
-    @name = name 
-    @breed = breed 
-    @age = age 
+    @name = name
+    @breed = breed
+    @age = age
   end
 end
